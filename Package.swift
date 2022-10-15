5b7ba20ca30884b8cd57c9123aa23f6532a6b3bf
Boris Barac <boris.raptor@gmail.com>
2022-10-15 23:25:02 +0200

    files moved

--- Package.swift
+++ Package.swift
@@ -21,6 +21,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apollographql/apollo-ios.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", .upToNextMajor(from: "1.9.0")),
+        .package(name: "Api", path: "./GraphQl/Api"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -28,6 +29,7 @@ let package = Package(
         .target(
             name: "RMGraphQL",
             dependencies: [
+                .product(name: "Api", package: "Api"),
                 .product(name: "Apollo", package: "apollo-ios"),
                 .product(name: "ApolloSQLite", package: "apollo-ios"),
                 .product(name: "SwiftyBeaver", package: "SwiftyBeaver"),
--- Sources/RMGraphQL/RMGraphQL.swift
+++ Sources/RMGraphQL/RMGraphQL.swift
@@ -2,11 +2,17 @@ import Foundation
 import Apollo
 import ApolloSQLite
 import SwiftyBeaver
+import Api
 
 
 public struct RMGraphQL {
     public private(set) var text = "Hello, World!"
 
     public init() {
+
+        let l: Unions?
+        let ll: DeadCharactersQuery?
+
     }
+
 }
