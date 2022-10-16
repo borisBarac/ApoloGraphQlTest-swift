b605016497ef0ae464718706062582b158f7cffd
Boris Barac <boris.raptor@gmail.com>
2022-10-16 19:37:59 +0200

    Constants file added

---
+++ Sources/RMGraphQL/Constants.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+// fallback endpoint if not provided in config
+let rmApiEndpont = URL(string: "https://rickandmortyapi.graphcdn.app/")!
+
+public enum RMError: Error {
+    case asyncConvertionFailed
+}
