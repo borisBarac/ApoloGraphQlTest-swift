7ff261d74f680a7392ab4f39b7d1bb33d13a73e4
Boris Barac <boris.raptor@gmail.com>
2022-10-16 20:14:18 +0200

    Basic logger added

--- Sources/RMGraphQL/Logger.swift
+++ Sources/RMGraphQL/Logger.swift
@@ -1,11 +1,49 @@
 import Foundation
-import SwiftyBeaver
 
+public enum LoggingLevel: Int {
+    case none = 0
+    case error
+    case all
+}
 
-struct Logger {
+enum LogType {
+    case normal
+    case error
+    case important
+}
 
-    // priority
-    // priority mapping
-    // logger creation
+class Logger {
+    let loggingLevel: LoggingLevel
+    var history = [String]()
+
+    init(loggingLevel: LoggingLevel) {
+        self.loggingLevel = loggingLevel
+    }
+
+    func log(message: String, logType: LogType = .normal, file: String = #file, line: Int = #line) {
+        let startSymbol = startLogSymbol(logType)
+        let message = "\(startSymbol) \(file): line \(line) - \(message)"
+        history.append(message)
+
+        switch loggingLevel {
+        case .none:
+            return
+        case .all:
+            print(message)
+        case .error:
+            logType == .error ? print(message) : ()
+        }
+    }
+
+    private func startLogSymbol(_ logType: LogType) -> String {
+        switch logType {
+        case .normal:
+            return "ℹ️"
+        case .error:
+            return "🔴"
+        case .important:
+            return "‼️"
+        }
+    }
 
 }
